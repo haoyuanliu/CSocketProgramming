ec45dc91117533df1478e1369ddea298b1c709e0
haoyuanliu <15120101@bjtu.edu.cn>
2017-03-03 21:38:57 +0800

    update raw files

--- chat/RawServer.c
+++ chat/RawServer.c
@@ -42,38 +42,38 @@ char physical_port[30];
 
 //定义缓存大小
 #define BUFSIZE 1024*5
-char sendbuf[BUFSIZE]={0};
-char recvbuf[BUFSIZE]={0};
+char sendbuf[BUFSIZE] = {0};
+char recvbuf[BUFSIZE] = {0};
 
 //shengming
-void makepkg(char *hdr,char *buffer);
-void ethernet_setpormisc(int fd,int i_flags);
+void makepkg(char *hdr, char *buffer);
+void ethernet_setpormisc(int fd, int i_flags);
 time_t lt;
 //创建发送函数线程
 void *thread_send()
 {
     //首先建立socket
     int sock_send;
-    sock_send=socket(PF_PACKET,SOCK_PACKET,htons(ETH_P_ALL));
-    if(sock_send<0)
+    sock_send = socket(PF_PACKET, SOCK_PACKET, htons(ETH_P_ALL));
+    if(sock_send < 0)
     {
         perror("scoket created");
     }
 
     //设置发包地址
     struct sockaddr send_addr;
-    memset(&send_addr,0,sizeof(send_addr));
-    strcpy(send_addr.sa_data,physical_port);
-	char hdr[4]="TO_B";
+    memset(&send_addr, 0, sizeof(send_addr));
+    strcpy(send_addr.sa_data, physical_port);
+	char hdr[4] = "TO_B";
 
     //创建发送程序
-    while(fgets(sendbuf,sizeof(sendbuf),stdin)!=0)
+    while(fgets(sendbuf, sizeof(sendbuf), stdin) != 0)
     {
-		makepkg(hdr,sendbuf);
+		makepkg(hdr, sendbuf);
         //printf("%s",sendbuf);
-        int len=sendto(sock_send,&sendbuf,strlen(sendbuf),0,&send_addr,sizeof(send_addr));
+        int len = sendto(sock_send, &sendbuf, strlen(sendbuf), 0, &send_addr, sizeof(send_addr));
         //printf("message sent!\n");
-        memset(sendbuf,0,sizeof(sendbuf));
+        memset(sendbuf, 0, sizeof(sendbuf));
     }
     close(sock_send);
     pthread_exit(NULL);
@@ -83,33 +83,33 @@ void *thread_send()
 //创建一个接收的线程
 void *thread_recv()
 {
-	socklen_t recv_socket_len=0;
+	socklen_t recv_socket_len = 0;
     //首先创建一个原始套接字
     int sock_recv;
-    if((sock_recv=socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL)))<0)
+    if((sock_recv = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
     {
         perror("physicl socket created");
     }
 
     //设置网卡为混杂模式；
-    ethernet_setpormisc(sock_recv,1);
+    ethernet_setpormisc(sock_recv, 1);
 
 	//setsockopt
-	int recvbuf_size=BUFSIZE;
-	setsockopt(sock_recv,SOL_SOCKET,SO_RCVBUF,&recvbuf,sizeof(char));
+	int recvbuf_size = BUFSIZE;
+	setsockopt(sock_recv, SOL_SOCKET, SO_RCVBUF, &recvbuf, sizeof(char));
     //获取物理网卡接口索，用以传输数据
     struct ifreq ifr_recv;
-    strcpy(ifr_recv.ifr_name,physical_port);
-    if(ioctl(sock_recv,SIOCGIFINDEX,&ifr_recv)<0)
+    strcpy(ifr_recv.ifr_name, physical_port);
+    if(ioctl(sock_recv, SIOCGIFINDEX, &ifr_recv)<0)
     {
         perror("[3]get interface index");
     }
     //绑定物理网卡
     struct sockaddr_ll local_addr;
-    local_addr.sll_family=PF_PACKET;
-    local_addr.sll_ifindex=ifr_recv.ifr_ifindex;
-    local_addr.sll_protocol=htons(ETH_P_ALL);
-    if((bind(sock_recv,(struct sockaddr *)&local_addr,sizeof(local_addr)))<0)
+    local_addr.sll_family = PF_PACKET;
+    local_addr.sll_ifindex = ifr_recv.ifr_ifindex;
+    local_addr.sll_protocol = htons(ETH_P_ALL);
+    if((bind(sock_recv, (struct sockaddr *)&local_addr, sizeof(local_addr))) < 0)
     {
         perror("[4]bind physical address");
     }
@@ -117,15 +117,15 @@ void *thread_recv()
     //开始接收数据包
     while(1)
     {
-		bzero(recvbuf,BUFSIZE);
-        recvfrom(sock_recv,recvbuf,BUFSIZE,0,NULL,NULL);
-        if(recvbuf[0]=='T'&&recvbuf[1]=='O'&&recvbuf[2]=='_'&&recvbuf[3]=='A')
+		bzero(recvbuf, BUFSIZE);
+        recvfrom(sock_recv, recvbuf, BUFSIZE, 0, NULL, NULL);
+        if(recvbuf[0]=='T' && recvbuf[1]=='O' &&recvbuf[2]=='_' && recvbuf[3]=='A')
         {
-            printf("Recived:%s",recvbuf+4);
+            printf("Recived:%s", recvbuf+4);
 			printf("___________________");
-        	printf(ctime(&lt),stdout);
+        	printf(ctime(&lt), stdout);
         }
-		memset(recvbuf,0,sizeof(recvbuf));
+		memset(recvbuf, 0, sizeof(recvbuf));
     }
     exit(0);
     close(sock_recv);
@@ -134,18 +134,18 @@ void *thread_recv()
 
 
 //创建设置网卡混杂模式函数
-void ethernet_setpormisc(int fd,int i_flags)
+void ethernet_setpormisc(int fd, int i_flags)
 {
     //首先获取网卡接口标志位
     struct ifreq ifr_s;
 	//printf("1.%s\n\n",physical_port);
-    memcpy(ifr_s.ifr_name,physical_port,sizeof(physical_port));
+    memcpy(ifr_s.ifr_name, physical_port, sizeof(physical_port));
 	//printf("2.%s",physical_port);//there is a problem canot printf it
-    if(ioctl(fd,SIOCGIFFLAGS,&ifr_s)<0)
+    if(ioctl(fd, SIOCGIFFLAGS, &ifr_s) < 0)
     {
         perror("[1]get interface flags");
     }
-    if(i_flags==0)
+    if(i_flags == 0)
     {
         //取消混杂模式
         ifr_s.ifr_flags &= ~IFF_PROMISC;
@@ -157,43 +157,43 @@ void ethernet_setpormisc(int fd,int i_flags)
     }
 
     //将接口设置为相应的模式
-    if(ioctl(fd,SIOCSIFFLAGS,&ifr_s)<0)
+    if(ioctl(fd, SIOCSIFFLAGS, &ifr_s) < 0)
     {
         perror("[2]set interface flags");
     }
 }
 
 //创建一个makepackge的组包程序
-void makepkg(char *hdr,char *buffer)
+void makepkg(char *hdr, char *buffer)
 {
-    int i=0;
+    int i = 0;
     char temp[BUFSIZE];
-    memcpy(temp,sendbuf,BUFSIZE);
-    memcpy(sendbuf,hdr,sizeof(hdr));
-    memcpy((sendbuf+sizeof(hdr)),temp,sizeof(temp));
+    memcpy(temp, sendbuf, BUFSIZE);
+    memcpy(sendbuf, hdr, sizeof(hdr));
+    memcpy((sendbuf+sizeof(hdr)), temp, sizeof(temp));
 }
 
 
 //创建主函数
 void main()
 {
-	lt=time(NULL);
+	lt = time(NULL);
 
     //根据声明定义好网卡端口号
-    memcpy(physical_port,PHYSICALPORT,PHYSICALPORT_LEN);
+    memcpy(physical_port, PHYSICALPORT, PHYSICALPORT_LEN);
 
     //创建发送线程函数
     pthread_t pthread_send;
-    int fd_send=pthread_create(&pthread_send,NULL,thread_send,NULL);
-    if(fd_send<0)
+    int fd_send = pthread_create(&pthread_send, NULL, thread_send, NULL);
+    if(fd_send < 0)
     {
         perror("thread_send created");
     }
 
     //创建接收线程函数
     pthread_t pthread_recv;
-    int fd_recv=pthread_create(&pthread_recv,NULL,thread_recv,NULL);
-    if(fd_recv<0)
+    int fd_recv = pthread_create(&pthread_recv, NULL, thread_recv, NULL);
+    if(fd_recv < 0)
     {
         perror("thread_recv created");
     }
